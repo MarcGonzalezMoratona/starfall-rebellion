38b4f5713d5b04c11ebb9bd779ceb0a63df2690c
Marc González Moratona <80526953+MarcGonzalezMoratona@users.noreply.github.com>
2023-05-29 19:51:16 +0200

    fix: change 200 to 201 in newsletter http call and error handling

--- app/components/newsletter-signup-form.tsx
+++ app/components/newsletter-signup-form.tsx
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 export default function NewsLetterSignUpForm() {
   const inputRef = useRef<HTMLInputElement>(null);
   const [hasSubscribed, setSubscribed] = useState(false);
+  const [isError, setError] = useState(false);
 
   const subscribeUser = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -19,7 +20,8 @@ export default function NewsLetterSignUpForm() {
 
         method: "POST",
       });
-      if (res.status === 200) setSubscribed(true);
+      if (res.status === 201) setSubscribed(true);
+      else if (!res.ok) setError(true);
     }
   };
 
@@ -30,37 +32,44 @@ export default function NewsLetterSignUpForm() {
           You have successfully signed up to the newsletter!
         </p>
       ) : (
-        <form
-          onSubmit={subscribeUser}
-          className="flex flex-col gap-4 my-4 w-full"
-        >
-          <label
-            htmlFor="email-input"
-            className="text-white text-lg flex flex-col gap-2"
+        <div className="flex flex-col">
+          {isError && (
+            <p className="text-center w-full">
+              Ooops! Something went wrong. Please, try again later.
+            </p>
+          )}
+          <form
+            onSubmit={subscribeUser}
+            className="flex flex-col gap-4 my-4 w-full"
           >
-            <h2 className="text-2xl text-white font-bold">STAY CONNECTED!</h2>
-            <p>Sign up to the Starfall Rebellion newsletter.</p>
-          </label>
-          <input
-            className="rounded-md px-4 py-2 text-zinc-900"
-            type="email"
-            id="email-input"
-            name="email"
-            placeholder="Enter your email address"
-            ref={inputRef}
-            required
-            autoCapitalize="off"
-            autoCorrect="off"
-          />
-          <button
-            className="bg-amber py-2 text-black text-lg font-semibold rounded-md"
-            type="submit"
-            value=""
-            name="subscribe"
-          >
-            SUBMIT
-          </button>
-        </form>
+            <label
+              htmlFor="email-input"
+              className="text-white text-lg flex flex-col gap-2"
+            >
+              <h2 className="text-2xl text-white font-bold">STAY CONNECTED!</h2>
+              <p>Sign up to the Starfall Rebellion newsletter.</p>
+            </label>
+            <input
+              className="rounded-md px-4 py-2 text-zinc-900"
+              type="email"
+              id="email-input"
+              name="email"
+              placeholder="Enter your email address"
+              ref={inputRef}
+              required
+              autoCapitalize="off"
+              autoCorrect="off"
+            />
+            <button
+              className="bg-amber py-2 text-black text-lg font-semibold rounded-md"
+              type="submit"
+              value=""
+              name="subscribe"
+            >
+              SUBMIT
+            </button>
+          </form>
+        </div>
       )}
     </>
   );
