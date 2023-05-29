153bc7c50de1967ea47d21e29e6ddb6c6fee68bb
Marc González Moratona <80526953+MarcGonzalezMoratona@users.noreply.github.com>
2023-05-29 19:16:56 +0200

    styling: newsletter

--- app/components/newsletter-signup-form.tsx
+++ app/components/newsletter-signup-form.tsx
@@ -1,54 +1,67 @@
-import { useRef } from 'react';
+import { useRef, useState } from "react";
 
 export default function NewsLetterSignUpForm() {
   const inputRef = useRef<HTMLInputElement>(null);
+  const [hasSubscribed, setSubscribed] = useState(false);
 
   const subscribeUser = async (e: React.FormEvent) => {
     e.preventDefault();
 
     if (inputRef.current != null) {
-      const res = await fetch('/api/subscribeUser', {
+      const res = await fetch("/api/subscribeUser", {
         body: JSON.stringify({
-          email: inputRef.current.value || '',
+          email: inputRef.current.value || "",
         }),
 
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
 
-        method: 'POST',
+        method: "POST",
       });
+      if (res.status === 200) setSubscribed(true);
     }
   };
 
   return (
-    <form onSubmit={subscribeUser} className="flex flex-col gap-4 my-4 w-full">
-      <label
-        htmlFor="email-input"
-        className="text-white text-lg flex flex-col gap-2"
-      >
-        <h2 className="text-2xl text-white font-bold">STAY CONNECTED!</h2>
-        <p>Sign up to the Starfall Rebellion newsletter.</p>
-      </label>
-      <input
-        className="rounded-md px-4 py-2"
-        type="email"
-        id="email-input"
-        name="email"
-        placeholder="Enter your email address"
-        ref={inputRef}
-        required
-        autoCapitalize="off"
-        autoCorrect="off"
-      />
-      <button
-        className="bg-amber py-2 text-black text-lg font-semibold rounded-md"
-        type="submit"
-        value=""
-        name="subscribe"
-      >
-        SUBMIT
-      </button>
-    </form>
+    <>
+      {hasSubscribed ? (
+        <p className="text-center w-full">
+          You have successfully signed up to the newsletter!
+        </p>
+      ) : (
+        <form
+          onSubmit={subscribeUser}
+          className="flex flex-col gap-4 my-4 w-full"
+        >
+          <label
+            htmlFor="email-input"
+            className="text-white text-lg flex flex-col gap-2"
+          >
+            <h2 className="text-2xl text-white font-bold">STAY CONNECTED!</h2>
+            <p>Sign up to the Starfall Rebellion newsletter.</p>
+          </label>
+          <input
+            className="rounded-md px-4 py-2 text-zinc-900"
+            type="email"
+            id="email-input"
+            name="email"
+            placeholder="Enter your email address"
+            ref={inputRef}
+            required
+            autoCapitalize="off"
+            autoCorrect="off"
+          />
+          <button
+            className="bg-amber py-2 text-black text-lg font-semibold rounded-md"
+            type="submit"
+            value=""
+            name="subscribe"
+          >
+            SUBMIT
+          </button>
+        </form>
+      )}
+    </>
   );
 }
