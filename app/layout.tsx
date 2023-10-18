81c680ee8bf87e51f52e682ff6f53f8342cc97c7
marcelinus99 <marcgonzalezmoratona@gmail.com>
2023-10-19 00:17:27 +0200

    feat: structured data

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,9 +41,61 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
   },
   metadataBase: new URL('https://starfall-rebellion.vercel.app'),
+  alternates: {
+    canonical: '/',
+  },
   viewport: 'width=device-width, initial-scale=1',
   creator: 'Horizons Games',
   publisher: 'Horizons Games',
+};
+
+const jsonLd = {
+  '@context': 'http://schema.org',
+  '@type': 'VideoGame',
+  name: 'LEGO Star Wars: Starfall Rebellion',
+  description: `LEGO Star Wars: Starfall Rebellion is a two-player hack and slash game that takes place in Coltran,
+  a planet oppressed by an Evil Corporation, which is full of poverty and outlawness.`,
+  url: 'https://starfall-rebellion.vercel.app',
+  image: 'https://starfall-rebellion.vercel.app/starfallRebellionOST.png',
+  datePublished: '2023-11-17',
+  author: {
+    '@type': 'Organization',
+    name: 'Horizons Games',
+  },
+  genre: 'Action, Adventure',
+  gamePlatform: ['PC'],
+  sameAs: [
+    'https://twitter.com/horizons_games',
+    'https://www.instagram.com/horizons.games',
+    'https://www.youtube.com/@horizonsgames',
+    'https://www.tiktok.com/@horizonsgames',
+    'https://www.linkedin.com/company/horizons-games/about/',
+    'https://github.com/Horizons-Games',
+  ],
+  publisher: {
+    '@type': 'Organization',
+    name: 'Horizons Games',
+    legalName: 'Horizons Games',
+    url: 'https://horizons-games.vercel.app',
+    description: `Horizons Games is a AAA game development studio 
+    created with educational purposes by Master's Degree students from UPC School in Barcelona.`,
+    foundingDate: 2022,
+    logo: 'https://horizons-games.vercel.app/horizons.svg',
+    sameAs: [
+      'https://twitter.com/horizons_games',
+      'https://www.instagram.com/horizons.games',
+      'https://www.youtube.com/@horizonsgames',
+      'https://www.tiktok.com/@horizonsgames',
+      'https://www.linkedin.com/company/horizons-games/about/',
+      'https://github.com/Horizons-Games',
+    ],
+    contactPoint: {
+      '@type': 'ContactPoint',
+      contactType: 'customer service',
+      email: 'horizongamesbarcelona@gmail.com',
+      url: 'https://starfall-rebellion.vercel.app/contact',
+    },
+  },
 };
 
 export default function RootLayout({
@@ -60,6 +112,10 @@ export default function RootLayout({
           <PageContextProvider>
             <MenuContextProvider>
               <Header />
+              <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+              />
               {children}
               <Footer />
               <Analytics />
