f76a2baac8f28d62d7dd407f54c31cc8d65f16b3
marcelinus99 <marcgonzalezmoratona@gmail.com>
2023-10-20 18:33:34 +0200

    feat: add portfolios and linkedins

--- app/data/team.js
+++ app/data/team.js
@@ -1,71 +1,6 @@
 export const team = {
   partners: [
     {
-      name: 'David Castro',
-      image: 'DavidCastro.jpg',
-      primaryRole: 'Gameplay Programmer',
-      networks: {
-        github: 'https://github.com/DADSILVER',
-      },
-    },
-    {
-      name: 'Diego Mejía',
-      image: 'DiegoMejia.jpg',
-      primaryRole: 'Producer',
-      secondaryRole: '3D Artist',
-      networks: {
-        github: 'https://github.com/diegusmejia117',
-      },
-    },
-    {
-      name: 'Gerard Queralt',
-      image: 'GerardQueralt.jpg',
-      primaryRole: 'Lead Engine Programmer',
-      networks: {
-        github: 'https://github.com/gerard-queralt',
-        linkedin:
-          'https://www.linkedin.com/in/gerard-queralt-ferr%C3%A9-071283266',
-      },
-    },
-    {
-      name: 'Miquel Prieto',
-      image: 'MiquelPrieto.jpg',
-      primaryRole: 'Lead Graphics Programmer',
-      networks: {
-        github: 'https://github.com/MacXxs',
-        linkedin:
-          'https://www.linkedin.com/in/miquel-prieto-molin%C3%A9-a57a30ab',
-      },
-    },
-    {
-      name: 'Pedro González-Anta',
-      image: 'PeterGonzalez.jpg',
-      primaryRole: 'Engine Programmer',
-      secondaryRole: 'Gameplay Programmer',
-      networks: {
-        github: 'https://github.com/Piterga12',
-      },
-    },
-    {
-      name: 'Alexis Dieu',
-      image: 'AlexisDieu.jpg',
-      primaryRole: 'Graphics Programmer',
-      secondaryRole: 'Physics Programmer',
-      networks: {
-        github: 'https://github.com/AlexisDieu',
-        linkedin: 'https://www.linkedin.com/in/alexis-dieu-705ab81a3',
-      },
-    },
-    {
-      name: 'David Martín',
-      image: 'DavidMartin.jpg',
-      primaryRole: 'Lead Level Programmer',
-      secondaryRole: 'Physics Programmer',
-      networks: {
-        github: 'https://github.com/Dedado3000',
-      },
-    },
-    {
       name: 'Marc Alcón',
       image: 'MarcAlcon.jpg',
       primaryRole: 'Lead Gameplay Programmer',
@@ -76,14 +11,28 @@ export const team = {
       },
     },
     {
-      name: 'Sara Izquierdo',
-      image: 'SaraIzquierdo.jpg',
-      primaryRole: 'Producer',
-      secondaryRole: 'Engine Programmer',
+      name: 'Sandra Campaña',
+      image: 'SandraCampaña.jpg',
+      primaryRole: 'Gameplay Programmer',
+      secondaryRole: '2D Artist',
       networks: {
-        github: 'https://github.com/AuxPort66',
-        linkedin: 'https://www.linkedin.com/in/sara-izquierdo-elfau-285109222',
-        website: 'https://auxport66.itch.io/',
+        github: 'https://github.com/csandra24',
+      },
+    },
+    {
+      name: 'David Castro',
+      image: 'DavidCastro.jpg',
+      primaryRole: 'Gameplay Programmer',
+      networks: {
+        github: 'https://github.com/DADSILVER',
+      },
+    },
+    {
+      name: 'Pablo Cebollada',
+      image: 'PabloCebollada.jpg',
+      primaryRole: 'Graphics Programmer',
+      networks: {
+        github: 'https://github.com/PaablooCH',
       },
     },
     {
@@ -97,22 +46,25 @@ export const team = {
       },
     },
     {
-      name: 'Xavier Llubés',
-      image: 'XavierLlubes.jpg',
-      primaryRole: 'Physics Programmer',
+      name: 'Alexis Dieu',
+      image: 'AlexisDieu.jpg',
+      primaryRole: 'Graphics Programmer',
+      secondaryRole: 'Physics Programmer',
       networks: {
-        github: 'https://github.com/xavi1111',
+        github: 'https://github.com/AlexisDieu',
+        linkedin: 'https://www.linkedin.com/in/alexis-dieu-705ab81a3',
       },
     },
     {
-      name: 'Oriol Murcia',
-      image: 'OriolMurcia.jpg',
-      primaryRole: 'Lead Audio Programmer',
+      name: 'Pedro González-Anta',
+      image: 'PeterGonzalez.jpg',
+      primaryRole: 'Engine Programmer',
       secondaryRole: 'Gameplay Programmer',
       networks: {
-        github: 'https://github.com/UriMurcia',
-        linkedin: 'https://www.linkedin.com/in/oriol-murcia-catalan',
-        website: 'https://urimurcia.github.io',
+        github: 'https://github.com/Piterga12',
+        linkedin: 'https://www.linkedin.com/in/pedro-gatf',
+        website:
+          'https://portfolio-pedro-gonzalez-anta-torrents-faura.vercel.app',
       },
     },
     {
@@ -127,21 +79,82 @@ export const team = {
       },
     },
     {
-      name: 'Oriol Sors',
-      image: 'OriolSors.jpg',
-      primaryRole: 'Lead Physics Programmer',
+      name: 'Sara Izquierdo',
+      image: 'SaraIzquierdo.jpg',
+      primaryRole: 'Producer',
+      secondaryRole: 'Engine Programmer',
       networks: {
-        github: 'https://github.com/OriolSors',
-        linkedin: 'https://www.linkedin.com/in/oriol-sors-vidal-4361491b9',
+        github: 'https://github.com/AuxPort66',
+        linkedin: 'https://www.linkedin.com/in/sara-izquierdo-elfau-285109222',
+        website: 'https://auxport66.itch.io/',
       },
     },
     {
-      name: 'Sandra Campaña',
-      image: 'SandraCampaña.jpg',
-      primaryRole: 'Gameplay Programmer',
-      secondaryRole: '2D Artist',
+      name: 'Xavier Llubés',
+      image: 'XavierLlubes.jpg',
+      primaryRole: 'Physics Programmer',
       networks: {
-        github: 'https://github.com/csandra24',
+        github: 'https://github.com/xavi1111',
+      },
+    },
+    {
+      name: 'David Martín',
+      image: 'DavidMartin.jpg',
+      primaryRole: 'Lead Level Programmer',
+      secondaryRole: 'Physics Programmer',
+      networks: {
+        github: 'https://github.com/Dedado3000',
+        linkedin: 'https://www.linkedin.com/in/davidmartínsaladms',
+        website: 'https://dedado.carrd.co',
+      },
+    },
+    {
+      name: 'Alejandro Matus',
+      image: 'AlejandroMatus.jpg',
+      primaryRole: 'Lead UX/UI',
+      secondaryRole: 'Level Designer',
+      networks: {
+        github: 'https://github.com/Vlukkee',
+      },
+    },
+    {
+      name: 'Diego Mejía',
+      image: 'DiegoMejia.jpg',
+      primaryRole: 'Producer',
+      secondaryRole: '3D Artist',
+      networks: {
+        github: 'https://github.com/diegusmejia117',
+      },
+    },
+    {
+      name: 'Oriol Murcia',
+      image: 'OriolMurcia.jpg',
+      primaryRole: 'Lead Audio Programmer',
+      secondaryRole: 'Gameplay Programmer',
+      networks: {
+        github: 'https://github.com/UriMurcia',
+        linkedin: 'https://www.linkedin.com/in/oriol-murcia-catalan',
+        website: 'https://urimurcia.github.io',
+      },
+    },
+    {
+      name: 'Miquel Prieto',
+      image: 'MiquelPrieto.jpg',
+      primaryRole: 'Lead Graphics Programmer',
+      networks: {
+        github: 'https://github.com/MacXxs',
+        linkedin:
+          'https://www.linkedin.com/in/miquel-prieto-molin%C3%A9-a57a30ab',
+      },
+    },
+    {
+      name: 'Gerard Queralt',
+      image: 'GerardQueralt.jpg',
+      primaryRole: 'Lead Engine Programmer',
+      networks: {
+        github: 'https://github.com/gerard-queralt',
+        linkedin:
+          'https://www.linkedin.com/in/gerard-queralt-ferr%C3%A9-071283266',
       },
     },
     {
@@ -154,20 +167,12 @@ export const team = {
       },
     },
     {
-      name: 'Pablo Cebollada',
-      image: 'PabloCebollada.jpg',
-      primaryRole: 'Graphics Programmer',
+      name: 'Oriol Sors',
+      image: 'OriolSors.jpg',
+      primaryRole: 'Lead Physics Programmer',
       networks: {
-        github: 'https://github.com/PaablooCH',
-      },
-    },
-    {
-      name: 'Alejandro Matus',
-      image: 'AlejandroMatus.jpg',
-      primaryRole: 'Lead UX/UI',
-      secondaryRole: 'Level Designer',
-      networks: {
-        github: 'https://github.com/Vlukkee',
+        github: 'https://github.com/OriolSors',
+        linkedin: 'https://www.linkedin.com/in/oriol-sors-vidal-4361491b9',
       },
     },
   ],
