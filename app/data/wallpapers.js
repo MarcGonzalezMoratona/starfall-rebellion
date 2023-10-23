fe84139f8f5e245669949dbff9a0b21721231169
marcelinus99 <marcgonzalezmoratona@gmail.com>
2023-10-23 16:38:20 +0200

    feat: allura wallpaper

--- app/data/wallpapers.js
+++ app/data/wallpapers.js
@@ -3,4 +3,8 @@ export const wallpapers = [
     url: '/wallpapers/bixWallpaper',
     description: 'Bix - The main character',
   },
+  {
+    url: '/wallpapers/alluraWallpaper',
+    description: 'Allura - The second character',
+  },
 ];
