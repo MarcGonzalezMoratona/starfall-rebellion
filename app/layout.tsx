44729c730c822ab7c7bf9ea24a1f7a2f134bede1
Marc-Gonzalez-Moratona <80526953+MarcGonzalezMoratona@users.noreply.github.com>
2023-10-25 01:25:27 +0200

    fix: weights for roboto

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,10 @@ import { PageContextProvider } from './store/page-context';
 import { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/react';
 
-const roboto = Roboto({ subsets: ['latin'], weight: '400' });
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
+});
 
 export const metadata: Metadata = {
   title: {
