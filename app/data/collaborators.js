d01121f70617fd42c735047d2b89f851c5cf6fcd
marcelinus99 <marcgonzalezmoratona@gmail.com>
2023-11-07 14:18:25 +0100

    feat: add sound team social networks

--- app/data/collaborators.js
+++ app/data/collaborators.js
@@ -5,14 +5,20 @@ export const collaborators = {
       image: 'NicolasFranza.jpg',
       primaryRole: 'Composer',
       secondaryRole: 'Technical Sound Designer',
-      networks: {},
+      networks: {
+        linkedin: 'https://www.linkedin.com/in/nico-fff',
+        youtube: 'https://www.youtube.com/@azynf',
+      },
     },
     {
       name: 'Antonio Zimmerman',
       image: 'AntonioZimmerman.jpg',
       primaryRole: 'Composer',
       secondaryRole: 'Sound Designer',
-      networks: {},
+      networks: {
+        linkedin: 'https://www.linkedin.com/in/antonio-zimmerman-27a7ab1b',
+        youtube: 'https://www.youtube.com/@azynf',
+      },
     },
   ],
 };
