67ab113bcd415af5e59190bea420c7cf38d5d678
marcelinus99 <marcgonzalezmoratona@gmail.com>
2023-11-13 14:05:59 +0100

    feat: add pages to sitemap.xml

--- app/sitemap.js
+++ app/sitemap.js
@@ -4,16 +4,22 @@ const URL = 'https://starfall-rebellion.vercel.app';
 
 export default async function sitemap() {
   const postRoutes = posts.map(({ slug }) => ({
-    url: `${URL}/post/${slug}`,
+    url: `${URL}/news/${slug}`,
     lastModified: new Date().toISOString().split('T')[0],
   }));
 
-  const routes = ['', '/about', '/team', '/media', '/press', '/contact'].map(
-    (route) => ({
-      url: `${URL}${route}`,
-      lastModified: new Date().toISOString().split('T')[0],
-    })
-  );
+  const routes = [
+    '',
+    '/about',
+    '/team',
+    '/media',
+    '/press',
+    '/news',
+    '/contact',
+  ].map((route) => ({
+    url: `${URL}${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }));
 
   return [...routes, ...postRoutes];
 }
