4fa0d28cf0efb81640a51f5481977f89150e7e83
marcelinus99 <marcgonzalezmoratona@gmail.com>
2023-11-15 23:00:03 +0100

    feat: new songs

--- app/data/songs.js
+++ app/data/songs.js
@@ -6,17 +6,32 @@ export const songs = [
   },
   {
     id: 2,
-    title: 'Sewers - Level 1 (COMING SOON)',
+    title: 'Sewers - Level 1',
     file: 'sewers.wav',
   },
   {
     id: 3,
+    title: 'Hangar - Level 1 (COMING SOON)',
+    file: 'hangar.wav',
+  },
+  {
+    id: 4,
     title: 'Cargo Bay - Level 1',
     file: 'cargoBay.wav',
   },
   {
-    id: 4,
-    title: 'Station Interior - Level 2',
+    id: 5,
+    title: 'Spaceship Scape - Level 2',
+    file: 'spaceshipScape.wav',
+  },
+  {
+    id: 6,
+    title: 'Station Interior - Level 3',
     file: 'stationInterior.wav',
+  },
+  {
+    id: 7,
+    title: 'Final Boss - Level 4',
+    file: 'boss.wav',
   },
 ];
